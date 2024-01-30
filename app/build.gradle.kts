fc378d674738cb38f9b1ee431b8482bde33ffe66
rober <robergomez.gomez@gmail.com>
2024-01-30 17:43:31 +0100

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.example.dummyapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.dummyapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            resValue("string", "robername", "Dummy App")
+            buildConfigField("String", "BASE_URL", "\"https://dummyjson.com/\"")
+        }
+
+        getByName("debug") {
+            isDebuggable = true
+            resValue("string", "robername", "[DEBUG] Dummy App")
+            buildConfigField("String", "BASE_URL", "\"https://dummyjson.com/\"")
+//            buildConfigField("String", "BASE_URL", "\"https://newastro-debug.vercel.app/\"")  URL DEBUG
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    kotlin {
+        jvmToolchain(8)
+    }
+}
+
+dependencies {
+
+    val navVersion = "2.7.2"
+    val daggerVersion = "2.48"
+    val retrofitVersion = "2.9.0"
+
+    //    NavComponent
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+//    Dagger hilt
+    implementation("com.google.dagger:hilt-android:$daggerVersion")
+    kapt("com.google.dagger:hilt-compiler:$daggerVersion")
+
+//    Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
